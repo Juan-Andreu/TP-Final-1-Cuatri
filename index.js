d1e916fe2b1b18df220e433ff35177952aba2a11
Juan Andreu <juandreu2002@gmail.com>
2023-06-30 19:59:40 -0300

    Se añadieron imagenes para la galeria y se le dieron estios a la misma.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-document.getElementById('myForm').addEventListener('submit', function(value) {
-    value.preventDefault();   //Evita el envío del formulario si la validación falla
+// document.getElementById('myForm').addEventListener('submit', function(value) {
+//     value.preventDefault();   //Evita el envío del formulario si la validación falla
 
-    // Accedo a los campos del formulario y se muestran los valores en la consola
-    let name = document.getElementById('name').value;
-    let lastName = document.getElementById('lastName').value;
-    let email = document.getElementById('email').value;
+//     // Accedo a los campos del formulario y se muestran los valores en la consola
+//     let name = document.getElementById('name').value;
+//     let lastName = document.getElementById('lastName').value;
+//     let email = document.getElementById('email').value;
 
-    console.log('Nombre: ',name);
-    console.log('Apellido: ',lastName);
-    console.log('Email: ',email);
-})
+//     console.log('Nombre: ',name);
+//     console.log('Apellido: ',lastName);
+//     console.log('Email: ',email);
+// })
 
   
 
@@ -60,4 +60,7 @@ function agregarTarea(seccion) {
     }
   }
   
+  const $header = document.querySelector('.header');
+
+  // console.log($header);
   
